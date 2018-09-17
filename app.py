5e6118a82e2a94c86b6adf4af9646d890aaa7ed9
Matteo Casadei <matteo.casadei@gmail.com>
2018-09-17 14:43:17 +0200

    Unet improvement

--- app.py
+++ app.py
@@ -56,16 +56,14 @@ def build_net():
 
     p = tf.layers.conv2d(p, 512, config.kernel_size, kernel_initializer=initializer, padding="same",
                          activation=tf.nn.relu, name="conv-7")
-    p = tf.nn.max_pool(p, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    #p = tf.nn.max_pool(p, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-    p = tf.layers.conv2d(p, 1024, config.kernel_size, kernel_initializer=initializer, padding="same",
-                         activation=tf.nn.relu, name="conv-8")
+    #p = tf.layers.conv2d(p, 1024, config.kernel_size, kernel_initializer=initializer, padding="same", activation=tf.nn.relu, name="conv-8")
 
     bth_size = tf.placeholder(tf.int32, name="bth_size")
-    p = tf.nn.conv2d_transpose(p, filter=tf.Variable(tf.random_normal([3, 3, 512, 1024], mean=0.0, stddev=0.02)),
-                               output_shape=[bth_size, 7, 7, 512], strides=[1, 2, 2, 1], padding="SAME")
-    p = tf.nn.bias_add(p, tf.Variable(tf.random_normal([512], mean=0.0, stddev=0.02)))
-    p = tf.nn.relu(p)
+    #p = tf.nn.conv2d_transpose(p, filter=tf.Variable(tf.random_normal([3, 3, 512, 1024], mean=0.0, stddev=0.02)), output_shape=[bth_size, 7, 7, 512], strides=[1, 2, 2, 1], padding="SAME")
+    #p = tf.nn.bias_add(p, tf.Variable(tf.random_normal([512], mean=0.0, stddev=0.02)))
+    #p = tf.nn.relu(p)
 
     p = tf.nn.conv2d_transpose(p, filter=tf.Variable(tf.random_normal([3, 3, 256, 512], mean=0.0, stddev=0.02)),
                                output_shape=[bth_size, 13, 13, 256], strides=[1, 2, 2, 1], padding="SAME")
