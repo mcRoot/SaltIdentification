c2db9dc518d613317a32228c8b216699e9bd0008
Matteo Casadei <matteo.casadei@gmail.com>
2018-09-26 08:42:51 +0000

    Merged configuration

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ CACHE_PATH = os.path.abspath("./generated_data")
 MODEL_FILENAME = "nn_model.tf"
 
 save_model = False
-lovasz_epochs = 3
-use_lovasz_loss = True
+lovasz_epochs = -1
+use_lovasz_loss = False
 conv_to_rgb = False
 resize_image = False
 tta = True
@@ -35,14 +35,14 @@ img_shape = (img_size, img_size)
 n_out_layers = 1
 n_channels = 1
 kernel_size = [3, 3]
-epochs = 4400
+epochs = 25
 batch_size = 100
-display_steps = 100
-learning_rate = 0.001
+display_steps = 500
+learning_rate = 0.0001
 validation_perc = 0.06
 #dropout_rate = 0.2
 
-thresholds = [0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+thresholds = [0.4, 0.45, 0.47, 0.5, 0.55, 0.6, 0.7, 0.75]
 kaggle_thresholds = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
 
 pred_step = 2000
